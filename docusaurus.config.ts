a479bbfbec4823f6ff1610620047d49370396712
Arnav Angarkar <arnav.angarkar20@gmail.com>
2026-06-10 19:37:57 +0530

    docs: fix docusaurus baseUrl and remove redundant mintlify config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://voxbento.com',
-  baseUrl: '/',
+  baseUrl: '/voxbento/',
 
   organizationName: 'fossasia',
   projectName: 'voxbento',
