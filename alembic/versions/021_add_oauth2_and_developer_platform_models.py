dee86720493fd8ba7e525564f1809156262948a8
Arnav Angarkar <arnav.angarkar20@gmail.com>
2026-08-18 16:40:55 +0530

    fix: Resolve sqlite alter table in migration

--- alembic/versions/021_add_oauth2_and_developer_platform_models.py
+++ alembic/versions/021_add_oauth2_and_developer_platform_models.py
@@ -7,9 +7,9 @@ Create Date: 2026-08-18 16:31:58.383311
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '021'
