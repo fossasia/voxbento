76a908b41ad8745b3592e7ddc394c4755670a197
Nandini Sharma <sharmanandini3006@gmail.com>
2026-09-26 08:19:24 +0000

    chore: remove unused config variables

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,15 +5,5 @@ from __future__ import annotations
 from portal.config import Settings
 
 
-def test_effective_mediamtx_internal_base_fallback():
-    s = Settings(mediamtx_internal_base="", mediamtx_api_base="http://localhost:9997")
-    assert s.effective_mediamtx_internal_base == "http://localhost:9997"
-
-
-def test_effective_mediamtx_internal_base_override():
-    s = Settings(mediamtx_internal_base="http://mediamtx:8888")
-    assert s.effective_mediamtx_internal_base == "http://mediamtx:8888"
-
-
 def test_debug_defaults_false():
     assert Settings(_env_file=None).debug is False
